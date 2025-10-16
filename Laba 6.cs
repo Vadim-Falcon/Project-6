3a6e77d659be7a404a821dccd5fdb9f67da0a150
Vadim-Falcon <stepanenko190709@gmail.com>
2025-10-16 23:36:54 +0300

    Add files via upload

--- Laba 6.cs
+++ Laba 6.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+
+namespace Laba_6_KTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {   Console.WriteLine("Введiть число: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ви ввели число: " + num);   
+            }
+            catch (FormatException) 
+            {
+            
+                Console.WriteLine("Ви ввели не число");
+            }
+            
+               
+        }
+    }
+}   
